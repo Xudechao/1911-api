d6a0071054763233fe832983dea428ef11b7d93e
xudechao <you@example.com>
2020-07-20 12:27:33 +0000

    访问次数

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Str;
+use GuzzleHttp\Client;
 use DB;
+use App\Model\GoodsModel;
 
 class TestController extends Controller
 {
@@ -25,6 +29,52 @@ class TestController extends Controller
         $key = 'user_info1';
         $redis = Redis::hgetall($key);
         print_r($redis);
+    }
+
+    public function has()
+    {
+        $shop = "shop";
+        $str = "1";
+        Redis::lpush($shop,$str);
+
+    }
+
+    public function gethas()
+    {
+        $shop = "shop";
+        $key = Redis::lrange($shop, 1, -1);
+        $key = Redis::llen($shop);
+        if ($key) {
+            echo "商品还有".$key."件";
+            Redis::lpop($shop);
+        }
+        if(empty($key)){
+            echo "商品已卖完";exit;
+        }
+    }
+
+    public function goods(Request $request)
+    {
+        $goods_id = $request->get('id');
+        $key1 = 'count:view:goods:'.$goods_id;
+        echo Redis::incr($key1); echo '</br>';
+
+        $key = 'h:goods_info:'.$goods_id;
+        //先判断缓存
+        $goods_info = Redis::hgetAll($key);
+
+        if(empty($goods_info))
+        {
+            $goods = GoodsModel::select('goods_id','goods_sn','cat_id','goods_name')->find($goods_id);
+            //缓存到redis中去
+            $goods_info = $goods->toArray();
+            Redis::hMset($key,$goods_info);
+            echo "未缓存";
+            echo '<pre>';print_r($goods_info);echo '</pre>';die;
+        }else{
+            echo "缓存";
+            echo '<pre>';print_r($goods_info);echo '</pre>';die;
+        }
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,10 @@ Route::post('/user/reg','User\IndexController@reg');     //注册
 Route::post('/user/login','User\IndexController@login');      //登录
 Route::get('/user/center','User\IndexController@center');      //个人中心
 
+Route::get("/has","TestController@has");
+Route::get("/gethas","TestController@gethas");
 
+Route::get('/goods','TestController@goods');
 
 Route::get('test1','TestController@test1');
 Route::get('test2','TestController@test2');
